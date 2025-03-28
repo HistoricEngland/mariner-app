95397d6e96b829133f39e38f56c7306f49f2c5f8
Andy Jones <andrew.jones@historicengland.org.uk>
2025-03-28 09:43:18 +0000

    adds test for bng datatype

---
+++ tests/docker_test_settings.py
@@ -0,0 +1,39 @@
+"""
+ARCHES - a program developed to inventory and manage immovable cultural heritage.
+Copyright (C) 2013 J. Paul Getty Trust and World Monuments Fund
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+# import from the test_settings.py files in the swame folder first
+
+
+from mariner_app.settings import *
+from .test_settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": get_env_variable("PGDBNAME"),
+        "USER": get_env_variable("PGUSERNAME"),
+        "PASSWORD": get_env_variable("PGPASSWORD"),
+        "HOST": get_env_variable("PGHOST"),
+        "PORT": get_env_variable("PGPORT"),
+        "POSTGIS_TEMPLATE": "template_postgis",
+    }
+}
+
+ELASTICSEARCH_HTTP_PORT = ast.literal_eval(get_env_variable("ESPORT"))
+ELASTICSEARCH_HOSTS = [{"scheme": "http", "host": get_env_variable("ESHOST"), "port": ELASTICSEARCH_HTTP_PORT}]
